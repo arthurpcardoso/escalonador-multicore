bbfd11d9a9df83b9bbbc808dbb9554277f8263b1
arthurpcardoso <arthurpcardoso13@gmail.com>
2024-12-29 17:28:45 -0300

    feat: leitura dos inputs

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    // Entrada do n_cores e quantum
+    // formato: escalona <numero_de_cores> <quantum>
+
+    int n_cores, quantum;
+    char comando[10];
+
+     // Ler o comando completo
+    scanf("%s %d %d", comando, &n_cores, &quantum);
+
+    // le o arquivo entrada.txt
+    FILE *entrada = fopen("entrada.txt", "r");
+
+    // le as linhas do arquivo
+    char linha[100];
+    while (fgets(linha, 100, entrada) != NULL) {
+        char identificador[50], arquivo_executavel[50];
+        int tempo_inicio, prioridade;
+
+        sscanf(linha, "%s %s %d %d", identificador, arquivo_executavel, &tempo_inicio, &prioridade);
+    }
+    
+
     return 0;
 }
